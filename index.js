6afd2192ea55c243259aefaa67f6a354b2412234
ATISH01 <ab.atish.ab@gmail.com>
2022-04-30 21:35:45 +0600

    post data operation done

--- index.js
+++ index.js
@@ -56,7 +56,9 @@ async function run(){
         })
         //Add new Items
         app.post('/allItems',async(req,res)=>{
-            
+            const newItems = req.body;
+            const result = await itemsCollection.insertOne(newItems);
+            res.send(result);
         })
 
     }
