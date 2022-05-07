fbe747bb8b72c201d25a9e07a9e366488dc65aa8
ATISH01 <ab.atish.ab@gmail.com>
2022-05-07 20:58:14 +0600

    jwt done

--- index.js
+++ index.js
@@ -11,50 +11,56 @@ app.use(cors());
 app.use(express.json());
 
 //JWT Verification
-/* function JWTVerification(req,res,next){
-    const authHeaders=req.headers.authorize;
-    if(!authHeaders){
-        return res.status(401).send({message: "Access not valid"})
+function JWTVerification(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: "Access not valid" })
     }
-    const token=authHeaders.split(' ')[1];
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-        if(err){
-            return res.status(403).send({message: "Access not valid"});
+
+    const token = authHeader.split(' ')[1];
+
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Access not valid" });
         }
-        console.log('decoded',decoded);
-        req.decoded=decoded;
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+
     })
-    
+
     next();
-} */
+
+}
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uy572.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const itemsCollection = client.db('Electronics').collection('allItems');
         // get All Items
-        app.get('/allItems',async(req,res)=>{
-            const query ={};
+        app.get('/allItems', async (req, res) => {
+            const query = {};
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items)
         })
         //get single Item
-        app.get('/allItems/:id',async(req,res)=>{
+        app.get('/allItems/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const singleItem = await itemsCollection.findOne(query);
             res.send(singleItem)
         })
         //update
-        app.put('/allItems/:id', async(req, res) =>{
+        app.put('/allItems/:id', async (req, res) => {
             const id = req.params.id;
             const updatedUser = req.body;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
@@ -67,38 +73,47 @@ async function run(){
         })
 
         //Delete items
-        app.delete('/allItems/:id',async(req,res)=>{
+        app.delete('/allItems/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await itemsCollection.deleteOne(query);
             res.send(result)
         })
         //Add new Items
-        app.post('/allItems',async(req,res)=>{
+        app.post('/allItems', async (req, res) => {
             const newItems = req.body;
             const result = await itemsCollection.insertOne(newItems);
             res.send(result);
         })
         // get user matched Items
-        app.get('/myItems',async(req,res)=>{
-            
+        app.get('/myItems', JWTVerification, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            console.log(email);
-            const query = {email};
-            const cursor = itemsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            console.log(email)
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = itemsCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+
+
         })
-        
+        //Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ accessToken })
+        })
 
     }
-    finally{}
+    finally { }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Running Server')
 })
-app.listen(port,()=>{
-    console.log('Listening POrt',port);
+app.listen(port, () => {
+    console.log('Listening POrt', port);
 })
